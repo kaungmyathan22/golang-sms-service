1beaad505454199e3022ecea4f0768f9e23b5507
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-28 23:12:08 +0630

    feat(): gin srever setup

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	//initialize config
+	app := api.Config{Router: router}
+
+	//routes
+	app.Routes()
+
+	router.Run(":8000")
+}
